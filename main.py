e52a1e5dd286a785e93df9a87ad04b3c73cd7484
David Leacock <David.Leacock@Yardi.com>
2020-03-06 18:15:35 -0600

    Commiting all models and images and import code.

---
+++ main.py
@@ -0,0 +1,27 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, ZeroPadding2D, Activation, Input, concatenate
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import MaxPooling2D, AveragePooling2D
+from tensorflow.keras.layers import Concatenate
+from tensorflow.keras.layers import Lambda, Flatten, Dense
+from tensorflow.keras.initializers import glorot_uniform
+from tensorflow.keras.layers import Layer
+from tensorflow.keras import backend as K
+K.set_image_data_format("channels_first")
+
+
+import cv2
+import os
+import numpy as np
+from numpy import genfromtxt
+import pandas as pd
+import tensorflow as tf
+from utils import *
+import sys
+from inception_blocks import *
+
+np.set_printoptions(threshold=sys.maxsize)
+
+FRmodel = faceRecoModel(input_shape=(3, 96, 96))
+print("Total Params:", FRmodel.count_params())
